4fb76fb0129f2a5853997d47bb75f57954531919
acktyne <pierremarc.baril@gmail.com>
2021-04-26 10:30:37 -0400

    header_part1

--- quartier_animal/lib/main.dart
+++ quartier_animal/lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,9 +12,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Quartier Animal',
       theme: ThemeData(
-        fontFamily: 'Barlow',
+        hoverColor: Color.fromRGBO(10, 173, 59, 1),
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -31,6 +34,7 @@ class _MyHomePageState extends State<MyHomePage> {
   final double targetElevation = 3;
   double _elevation = 0;
   ScrollController _controller;
+  Color animal_green = Color.fromRGBO(110, 173, 59, 1);
 
   void _scrollListener() {
     double newElevation = _controller.offset > 1 ? targetElevation : 0;
@@ -55,46 +59,209 @@ class _MyHomePageState extends State<MyHomePage> {
     _controller?.dispose();
   }
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(10),
-      color: Color.fromRGBO(10, 173, 59, 1),
+      padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+      color: animal_green,
       child: Scaffold(
-        appBar: PreferredSize(
-          preferredSize: Size.fromHeight(130.0),
-          child: AppBar(
-            elevation: _elevation,
-            backgroundColor: Color.fromRGBO(255, 255, 255, 1),
-            title: Text(widget.title,
-                style: TextStyle(fontFamily: 'Barlow', color: Colors.black)),
-          ),
+        appBar: AppBar(
+          toolbarHeight: 130,
+          elevation: _elevation,
+          backgroundColor: Colors.white,
+          // title: Text(widget.title,
+          //     style: TextStyle(fontFamily: 'Barlow', color: Colors.black)),
+          actions: [
+            Expanded(child: _header()),
+          ],
         ),
         body: SingleChildScrollView(
           controller: _controller,
           child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'You have pushed the button this many times:',
-                  style: TextStyle(fontFamily: 'Barlow'),
-                ),
-                Text(
-                  '$_counter',
-                  style: Theme.of(context).textTheme.headline4,
-                ),
-              ],
+            child: Container(
+              width: 200,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+                    style: TextStyle(fontFamily: 'Barlow'),
+                  ),
+                  Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+                    style: TextStyle(fontFamily: 'Barlow'),
+                  ),
+                  Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+                    style: TextStyle(fontFamily: 'Barlow'),
+                  ),
+                  Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+                    style: TextStyle(fontFamily: 'Barlow'),
+                  ),
+                  Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+                    style: TextStyle(fontFamily: 'Barlow'),
+                  ),
+                  Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+                    style: TextStyle(fontFamily: 'Barlow'),
+                  ),
+                  Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+                    style: TextStyle(fontFamily: 'Barlow'),
+                  ),
+                  Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+                    style: TextStyle(fontFamily: 'Barlow'),
+                  ),
+                  Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+                    style: TextStyle(fontFamily: 'Barlow'),
+                  ),
+                  Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+                    style: TextStyle(fontFamily: 'Barlow'),
+                  ),
+                  Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+                    style: TextStyle(fontFamily: 'Barlow'),
+                  ),
+                  Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+                    style: TextStyle(fontFamily: 'Barlow'),
+                  ),
+                  Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+                    style: TextStyle(fontFamily: 'Barlow'),
+                  ),
+                  Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+                    style: TextStyle(fontFamily: 'Barlow'),
+                  ),
+                  Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+                    style: TextStyle(fontFamily: 'Barlow'),
+                  ),
+                  Text(
+                    '$_counter',
+                    style: Theme.of(context).textTheme.headline4,
+                  ),
+                ],
+              ),
             ),
           ),
         ),
       ),
+    );
+  }
+
+  _header() {
+    return Column(
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(),
+            Container(
+              width: 960,
+              height: 130,
+              child: Row(
+                children: [
+                  Container(
+                    child: Image.asset(
+                      "assets/images/logo.png",
+                      height: 80,
+                      width: 238,
+                    ),
+                  ),
+                  Container(
+                      width: 722,
+                      child: Column(
+                        children: [
+                          Container(
+                            height: 50,
+                            width: 191,
+                            color: animal_green,
+                            child: TextButton(
+                                style: ButtonStyle(
+                                    overlayColor:
+                                        MaterialStateColor.resolveWith(
+                                            (states) => animal_green)),
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => MyHomePage()),
+                                  );
+                                },
+                                child: RichText(
+                                    text: TextSpan(
+                                        text: "BOUTIQUE EN LIGNE",
+                                        style: GoogleFonts.barlow(
+                                            fontWeight: FontWeight.w700,
+                                            textStyle: TextStyle(
+                                              color: Colors.white,
+                                              height: 2,
+                                              fontSize: 16,
+                                              fontWeight: FontWeight.bold,
+                                            ))))),
+                          ),
+                          Container(
+                            width: 634,
+                            height: 53,
+                            child: Row(
+                              children: [
+                                TextButton(
+                                    style: ButtonStyle(
+                                        foregroundColor:
+                                            MaterialStateProperty.all(
+                                                animal_green)),
+                                    onPressed: () {
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) => MyHomePage()),
+                                      );
+                                    },
+                                    child: RichText(
+                                        text: TextSpan(
+                                            text: "LA CLINIQUE",
+                                            style: TextStyle(
+                                                color: Colors.grey)))),
+                                TextButton(
+                                    onPressed: null,
+                                    child: RichText(
+                                        text: TextSpan(
+                                            text: "LA BOUTIQUE",
+                                            style: TextStyle(
+                                                color: Colors.grey)))),
+                                TextButton(
+                                    onPressed: null,
+                                    child: RichText(
+                                        text: TextSpan(
+                                            text: "LES SOINS",
+                                            style: TextStyle(
+                                                color: Colors.grey)))),
+                                TextButton(
+                                    onPressed: null,
+                                    child: RichText(
+                                        text: TextSpan(
+                                            text: "NOUS JOINDRE",
+                                            style: TextStyle(
+                                                color: Colors.grey)))),
+                              ],
+                            ),
+                          ),
+                        ],
+                      )),
+                ],
+              ),
+            ),
+            Container()
+          ],
+        ),
+      ],
     );
   }
 }
